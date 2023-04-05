fb5a7dd0e26a48808e7a024b90fe1c8e279f5fb3
Lucassimaog6 <22101918@aluno.cotemig.com.br>
2023-04-05 20:24:16 -0300

    Make localstorage work

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-const btnCadastrar = document.getElementById("cadastrar");
-const btnExcluir = document.getElementById("excluir");
-const btnEditar = document.getElementById("editar");
+const btnCadastrar = document.getElementById('cadastrar');
+const btnExcluir = document.getElementById('excluir');
+const btnEditar = document.getElementById('editar');
 
-const inputName = document.getElementById("name");
-const inputAmount = document.getElementById("amount");
-const inputPrice = document.getElementById("price");
-const inputShelf = document.getElementById("shelf");
-const inputDescription = document.getElementById("description");
-const inputCategory = document.getElementById("category");
+const inputName = document.getElementById('name');
+const inputAmount = document.getElementById('amount');
+const inputPrice = document.getElementById('price');
+const inputShelf = document.getElementById('shelf');
+const inputDescription = document.getElementById('description');
+const inputCategory = document.getElementById('category');
 
-const tbody = document.querySelector("tbody");
-let BD = [];
+const tbody = document.querySelector('tbody');
+let BD = localStorage.getItem('BD') ? JSON.parse(localStorage.getItem('BD')) : [];
+ReloadTable();
+
 let InEdit = null;
 
-btnCadastrar.addEventListener("click", () => {
+btnCadastrar.addEventListener('click', () => {
+	if (!CheckInput()) {
+		alert('Preencha todos os campos!');
+		return;
+	}
+
 	const product = {
 		id: BD.length,
 		nome: inputName.value,
@@ -29,29 +36,20 @@ btnCadastrar.addEventListener("click", () => {
 	ReloadTable();
 });
 
-btnExcluir.onclick = function () {
-	console.log("oi");
+btnExcluir.onclick = () => {
+	console.log('oi');
 	BD = BD.filter((e) => !e.checked);
 	ReloadTable();
+	Limpar();
 };
 
-function ReloadTable() {
-	tbody.innerHTML = null;
-	BD.forEach((e, i) => {
-		tbody.innerHTML += `
-            <tr class="text-center">
-                <td>
-                    <input type="checkbox" id="${i}" ${e.checked ? "checked" : null} onchange="CheckProduct(${i})">
-                </td>
-                <td>${e.nome}</td>
-                <td>${e.quantidade}</td>
-                <td>${e.preco}</td>
-            </tr>`;
-	});
-}
-
-btnEditar.onclick = function () {
+btnEditar.onclick = () => {
 	if (InEdit === null) {
+		return;
+	}
+
+	if (!CheckInput()) {
+		alert('Preencha todos os campos!');
 		return;
 	}
 
@@ -63,20 +61,70 @@ btnEditar.onclick = function () {
 		prateleira: inputShelf.value,
 		descricao: inputDescription.value,
 		categoria: inputCategory.value,
-		checked: false,
+		checked: BD[InEdit].checked,
 	};
 
 	BD[InEdit] = product;
 	ReloadTable();
+	Limpar();
 };
 
-const CheckProduct = (id) => {
-	BD[id].checked = !BD[id].checked;
-	InEdit = id;
-	inputName.value = BD[id].nome;
-	inputAmount.value = BD[id].quantidade;
-	inputPrice.value = BD[id].preco;
-	inputShelf.value = BD[id].prateleira;
-	inputDescription.value = BD[id].descricao;
-	inputCategory.value = BD[id].categoria;
-};
+function CheckProduct(id) {
+	if (BD[id].checked) {
+		InEdit = null;
+		Limpar();
+		BD[id].checked = false;
+	} else {
+		InEdit = id;
+		inputName.value = BD[id].nome;
+		inputAmount.value = BD[id].quantidade;
+		inputPrice.value = BD[id].preco;
+		inputShelf.value = BD[id].prateleira;
+		inputDescription.value = BD[id].descricao;
+		inputCategory.value = BD[id].categoria;
+		BD[id].checked = true;
+	}
+}
+
+function ReloadTable() {
+	tbody.innerHTML = null;
+	BD.forEach((e, i) => {
+		tbody.innerHTML += `
+            <tr class="text-center">
+                <td>
+                    <input type="checkbox" id="${i}" ${e.checked ? 'checked' : null} onchange="CheckProduct(${i})">
+                </td>
+                <td>${e.nome}</td>
+                <td>${e.quantidade}</td>
+                <td>${e.preco}</td>
+                <td>${e.descricao}</td>
+                <td>${e.prateleira}</td>
+                <td>${e.categoria}</td>
+            </tr>`;
+	});
+	localStorage.setItem('BD', JSON.stringify(BD));
+}
+
+function Limpar() {
+	inputName.value = '';
+	inputAmount.value = '';
+	inputPrice.value = '';
+	inputShelf.value = '';
+	inputDescription.value = '';
+	inputCategory.value = '';
+}
+
+function CheckInput() {
+	if (
+		inputName.value === '' ||
+		inputAmount.value === '' ||
+		inputPrice.value === '' ||
+		inputShelf.value === '' ||
+		inputDescription.value === '' ||
+		inputCategory.value === ''
+	) {
+		return false;
+	} else {
+		return true;
+	}
+}
